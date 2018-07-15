0a9bd808900219626701da804d9603241dba0428
kimnamho <kimnamho@192.168.0.45>
2018-07-15 15:31:03 +0900

    package newProejct;
    public class GitHubTest {
    	String a ;
    
    	public void Test(String b) {
    		this.a = b;
    		System.out.println(b);
    
    	}
    
    
    	public static void main(String args[]) {
    		GitHubTest a = new GitHubTest();
    		a.Test("안녕하세요");
    
    	}
    
    }

---
+++ final Test/src/Fianltest.java
@@ -0,0 +1,22 @@
+
+public class Fianltest {
+	String a ;
+		
+	public void Test(String b) {
+		this.a = b;
+		System.out.println(b);
+			
+		}
+		
+
+	public static void main(String args[]) {
+		Finaltest a =  new Finaltest();
+			
+		a.Test("안녕하세요");
+			
+		}
+
+	}
+
+
+
